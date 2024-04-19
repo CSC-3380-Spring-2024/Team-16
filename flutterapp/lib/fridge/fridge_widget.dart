52162a27c841d42580c70177496f86466ca6770e
Hong-Bin-Li <hongbin208@gmail.com>
2024-04-19 00:12:34 -0500

    Changed the create page and updated ui of it

--- flutterapp/lib/fridge/fridge_widget.dart
+++ flutterapp/lib/fridge/fridge_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
