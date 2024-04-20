bb631ed75b02934d1ec5a11465aed293b866fb76
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2024-04-19 23:32:24 -0500

    Account "authentication"
    all user credentials are stored in plaintext sorry

--- foodApp/src/main/java/com/example/foodApp/AccountCreaete/AccountController.java
+++ foodApp/src/main/java/com/example/foodApp/AccountCreaete/AccountController.java
@@ -2,6 +2,7 @@ package com.example.foodApp.AccountCreaete;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,23 @@ public class AccountController {
 
         return ResponseEntity.unprocessableEntity().body(createAccount);
     }
+    @PostMapping("/login")
+    public ResponseEntity<String> login (@RequestBody  Map<String, String> payload){
+        String username = payload.get("username");
+        String password = payload.get("password");
+
+        if(accountService.acountAuthy(username,password))
+        {
+            return ResponseEntity.ok().body("welcome "+ username);
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+
+    }
+
+
+    //GetMapping
 }
 
--- foodApp/src/main/java/com/example/foodApp/AccountCreaete/AccountService.java
+++ foodApp/src/main/java/com/example/foodApp/AccountCreaete/AccountService.java
@@ -3,6 +3,9 @@ package com.example.foodApp.AccountCreaete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
 
 @Service
 public class AccountService {
@@ -23,5 +26,11 @@ public class AccountService {
             mongoTemplate.insert(account);
             return "Account Successfully Created";
         }
+    }
+    public boolean acountAuthy (String username, String password)
+    {
+        Query query = new Query(Criteria.where("username").is(username).and("password").is(password));
+
+        return mongoTemplate.exists(query,Account.class);
     }
 }
