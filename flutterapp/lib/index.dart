a2b2d2d288ed3272c6b912420e76fb77bc25e27b
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2024-04-25 18:42:26 -0500

    Delete flutterapp folder from Backend branch

--- flutterapp/lib/index.dart
+++
@@ -1,5 +0,0 @@
-// Export pages
-export '/home_page/home_page_widget.dart' show HomePageWidget;
-export '/create/create_widget.dart' show CreateWidget;
-export '/fridge/fridge_widget.dart' show FridgeWidget;
-export '/account/account_widget.dart' show AccountWidget;
